5125335c126a8dc196234f2d47ec880f0f8b29fb
VarunGBhat <varungbhat@gmail.com>
2025-08-18 21:31:29 +0530

    changes to host the frontend on github pages

--- client/app/router.tsx
+++ client/app/router.tsx
@@ -377,7 +377,7 @@
 // );
 
 // export default AppRouter;
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "@/pages/Home/Home";
 import Login from "./pages/Login/Login";
@@ -482,7 +482,7 @@ const AppRouter = () => (
   //   </Routes>
   // </Router>
 
-  <Router>
+  <Router basename="/cnerlab">
     <Routes>
       <Route path="/" element={<RootWrapper />}>
         {/* Main app layout */}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,19 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./app"),
-//     },
-//   },
-//   base: "/cnerlabs/",
-//   server: {
-//     open: true,
-//     port: 3000,
-//   },
-// });
-
 export default defineConfig({
-  base: "/cnerlabs/", // Important for GitHub Pages
+  base: "/cnerlab/", // Updated to match your repository name
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./app"),
+      "@": "/app",
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
   },
 });
