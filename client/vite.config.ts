0212cc17956c5ae3c7b2a1bc8dc7b4908157a739
VarunGBhat <varungbhat@gmail.com>
2025-08-23 11:39:53 +0530

    vercel host fix #3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": "/app",
+      "@": path.resolve(__dirname, "./app"),
     },
   },
   build: {
